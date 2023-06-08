71cf20c141aac6a78be6dc3095ed564e6ba9cbc8
anton22071978 <121187741+anton22071978@users.noreply.github.com>
2023-06-08 22:57:44 +0300

    Update lesson_2_task_0.py

--- lesson_2_task_0.py
+++ lesson_2_task_0.py
@@ -1,5 +1,4 @@
-my_age = input ("Ваш возраст:my_age")
-my_age = int ()
+my_age = input("Ваш возраст:")
+my_age = int(my_age)
 my_age = my_age + 1
-my_age = int ('44')
-print (my_age)
+print(my_age) 
